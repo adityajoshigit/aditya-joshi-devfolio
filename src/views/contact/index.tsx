0011765cb9861667206a86d495dda1e2de31f008
adityajoshigit <adityasjoshi96@gmail.com>
2024-04-02 00:07:30 -0400

    removed unnecessary code

--- src/views/contact/index.tsx
+++ src/views/contact/index.tsx
@@ -14,62 +14,8 @@ export default React.forwardRef(
       <section
         ref={ref}
         id={sectionId}
-        className="mb-8">
+        className="mb-8 w-full">
         <SectionHeader label="Contact Me" />
-        <p className="mt-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Nulla vitae est eget turpis accumsan
-          vulputate.
-        </p>
-        <p className="mt-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Nulla vitae est eget turpis accumsan
-          vulputate.
-        </p>
-        <p className="mt-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Nulla vitae est eget turpis accumsan
-          vulputate. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odio magnam quibusdam recusandae
-          consequuntur cum nisi. Laudantium cumque adipisci
-          nihil sunt atque, eveniet aspernatur! Delectus
-          quas eius iusto laudantium adipisci tempora. Lorem
-          ipsum dolor sit amet consectetur adipisicing elit.
-          Voluptatem pariatur error, sapiente officia eaque
-          voluptatum adipisci ea est asperiores vel tempora
-          magni deserunt cumque saepe perspiciatis natus.
-          Debitis, eum perferendis!Lorem Lorem ipsum dolor,
-          sit amet consectetur adipisicing elit. Eius
-          blanditiis dolor veniam porro odit earum quod?
-          Quod voluptas, praesentium fugiat iure laboriosam
-          eius quas, nisi expedita minus cupiditate,
-          dignissimos neque.
-        </p>
-        <p className="mt-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Nulla vitae est eget turpis accumsan
-          vulputate.
-        </p>
-        <p className="mt-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Nulla vitae est eget turpis accumsan
-          vulputate. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odio magnam quibusdam recusandae
-          consequuntur cum nisi. Laudantium cumque adipisci
-          nihil sunt atque, eveniet aspernatur! Delectus
-          quas eius iusto laudantium adipisci tempora. Lorem
-          ipsum dolor sit amet consectetur adipisicing elit.
-          Voluptatem pariatur error, sapiente officia eaque
-          voluptatum adipisci ea est asperiores vel tempora
-          magni deserunt cumque saepe perspiciatis natus.
-          Debitis, eum perferendis!Lorem Lorem ipsum dolor,
-          sit amet consectetur adipisicing elit. Eius
-          blanditiis dolor veniam porro odit earum quod?
-          Quod voluptas, praesentium fugiat iure laboriosam
-          eius quas, nisi expedita minus cupiditate,
-          dignissimos neque.
-        </p>
-        {/* Add more content for Contact Me section */}
       </section>
     );
   }
