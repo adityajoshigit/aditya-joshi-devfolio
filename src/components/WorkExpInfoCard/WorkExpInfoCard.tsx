de1708b2df40ab0b126d8928d71681d7e529e099
adityajoshigit <adityasjoshi96@gmail.com>
2024-05-12 01:43:34 -0400

    updated component styling

--- src/components/WorkExpInfoCard/WorkExpInfoCard.tsx
+++ src/components/WorkExpInfoCard/WorkExpInfoCard.tsx
@@ -20,53 +20,51 @@ function WorkExpInfoCard({
   return (
     <div
       className={classNames(
-        "group min-h-fit flex ",
-        className,
-        {
-          "flex-row": align === "left",
-          "flex-row-reverse items-start ":
-            align === "right",
-        }
+        "group min-h-fit grid grid-cols-1 gap-1",
+        className
       )}>
-      <div
-        className={classNames(
-          `w-1/2 p-2 flex flex-col 
+      <div className="gap-0 grid grid-cols-2">
+        <div
+          className={classNames(
+            `p-2 flex flex-col 
           group-hover:bg-cyan-400 
           group-hover:text-white
           transition duration-200
           justify-center 
           `,
-          {
-            " items-end  border-r border-r-cyan-600":
-              align === "left",
-            " items-start  border-l border-l-cyan-600":
-              align === "right",
-          }
-        )}>
-        <span>{role}</span>
-        <p className="text-ellipsis">{company}</p>
-        <span>{tenure}</span>
-      </div>
-      <div className="group-hover:bg-gray-100 p-2 w-1/2 transition duration-200">
-        <ShowMoreLayout className="text-gray-500 text-sm">
-          Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Nulla vitae est eget turpis accumsan
-          vulputate. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odio magnam quibusdam recusandae
-          consequuntur cum nisi. Laudantium cumque adipisci
-          nihil sunt atque, eveniet aspernatur! Delectus
-          quas eius iusto laudantium adipisci tempora. Lorem
-          ipsum dolor sit amet consectetur adipisicing elit.
-          Voluptatem pariatur error, sapiente officia eaque
-          voluptatum adipisci ea est asperiores vel tempora
-          magni deserunt cumque saepe perspiciatis natus.
-          Debitis, eum perferendis!Lorem Lorem ipsum dolor,
-          sit amet consectetur adipisicing elit. Eius
-          blanditiis dolor veniam porro odit earum quod?
-          Quod voluptas, praesentium fugiat iure laboriosam
-          eius quas, nisi expedita minus cupiditate,
-          dignissimos neque.
-        </ShowMoreLayout>
+            {
+              " items-end  border-r border-r-cyan-600":
+                align === "left",
+              " items-start  border-l border-l-cyan-600":
+                align === "right",
+            }
+          )}>
+          <span>{role}</span>
+          <p className="text-ellipsis">{company}</p>
+          <span>{tenure}</span>
+        </div>
+        <div className="group-hover:bg-gray-100 p-2 transition duration-200">
+          <ShowMoreLayout className="text-gray-500 text-sm">
+            Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Nulla vitae est eget turpis
+            accumsan vulputate. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Odio magnam
+            quibusdam recusandae consequuntur cum nisi.
+            Laudantium cumque adipisci nihil sunt atque,
+            eveniet aspernatur! Delectus quas eius iusto
+            laudantium adipisci tempora. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit.
+            Voluptatem pariatur error, sapiente officia
+            eaque voluptatum adipisci ea est asperiores vel
+            tempora magni deserunt cumque saepe perspiciatis
+            natus. Debitis, eum perferendis!Lorem Lorem
+            ipsum dolor, sit amet consectetur adipisicing
+            elit. Eius blanditiis dolor veniam porro odit
+            earum quod? Quod voluptas, praesentium fugiat
+            iure laboriosam eius quas, nisi expedita minus
+            cupiditate, dignissimos neque.
+          </ShowMoreLayout>
+        </div>
       </div>
     </div>
   );
--- src/views/work-exp/WorkExpView.tsx
+++ src/views/work-exp/WorkExpView.tsx
@@ -5,16 +5,25 @@ function WorkExpView() {
   return (
     <div className="gap-0 grid grid-cols-1">
       {workExpData.map(
-        ({ role, company, tenure }, index) => (
-          <WorkExpInfoCard
-            role={role}
-            company={company}
-            tenure={tenure}
-            align={
-              // (index + 1) % 2 === 0 ? "right" : "left"
-              "left"
-            }></WorkExpInfoCard>
-        )
+        ({ role, company, tenure }, index) => {
+          return (
+            <>
+              <WorkExpInfoCard
+                role={role}
+                company={company}
+                tenure={tenure}
+                align={
+                  // (index + 1) % 2 === 0 ? "right" : "left"
+                  "left"
+                }></WorkExpInfoCard>
+              {/* {index !== workExpData.length - 1 ? (
+                <div className="flex justify-center items-center w-full">
+                  <div className="bg-cyan-600 rounded-lg w-2 h-2 translate-x-1/2"></div>
+                </div>
+              ) : null} */}
+            </>
+          );
+        }
       )}
     </div>
   );
