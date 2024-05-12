76034d04ae18d782823c90c194f58bd85d1fcb71
adityajoshigit <adityasjoshi96@gmail.com>
2024-05-12 01:19:35 -0400

    updated styling in show more layout

--- src/components/ShowMoreLayout/ShowMoreLayout.tsx
+++ src/components/ShowMoreLayout/ShowMoreLayout.tsx
@@ -2,10 +2,12 @@ import classNames from "classnames";
 import { ReactNode, useState } from "react";
 
 export interface IShowMoreLayoutProps {
+  className?: string;
   children?: string | ReactNode;
 }
 
 function ShowMoreLayout({
+  className = "",
   children,
 }: IShowMoreLayoutProps) {
   const [isMoreShown, setIsMoreShown] = useState(false);
@@ -17,7 +19,7 @@ function ShowMoreLayout({
     <>
       <div
         className={classNames(
-          "rounded-lg ",
+          className,
           isMoreShown ? "" : "h-[100px] overflow-hidden"
         )}>
         <div className="inset-0 flex flex-col justify-start items-center pb-2">
@@ -32,7 +34,7 @@ function ShowMoreLayout({
       </div>
       <div className="text-right w-full">
         <button
-          className="text-gray-600 hover:underline italic hover:cursor-pointer"
+          className="text-neutral-500 text-sm hover:underline italic hover:cursor-pointer"
           onClick={() => toggleState()}>
           {isMoreShown ? "Show Less" : "Show More"}
         </button>
