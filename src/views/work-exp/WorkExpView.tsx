e999071f3dd9e363d36e2d200f9a17a0d631385e
adityajoshigit <adityasjoshi96@gmail.com>
2024-05-12 00:39:29 -0400

    created a basic view for work exp

--- src/views/work-exp/WorkExpView.tsx
+++ src/views/work-exp/WorkExpView.tsx
@@ -2,8 +2,11 @@ import { WorkExpInfoCard } from "../../components";
 
 function WorkExpView() {
   return (
-    <div>
-      <WorkExpInfoCard></WorkExpInfoCard>
+    <div className="gap-0 grid grid-cols-1">
+      <WorkExpInfoCard
+        role={"Role"}
+        company={"Company"}
+        tenure={"Tenure"}></WorkExpInfoCard>
     </div>
   );
 }
